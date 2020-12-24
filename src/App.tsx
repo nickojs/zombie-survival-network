4800f7ac183e690606819f9444d5694fef05cb3a
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-24 12:30:06 -0300

    feat(app): import Notification component on main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Auth from './Containers/Auth';
 import Title from './components/UI/text';
+import Notification from './components/notification';
 
 function App() {
   return (
     <>
+      <Notification />
       <Title text="S.N.I. - Survival Network Interface" />
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Auth />
