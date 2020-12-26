80159f8fc45f4856a883d4e4a4ed4491cf8ba4a9
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-26 16:48:58 -0300

    build: add 'react-router-dom' lib

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { NotificationProvider } from './contexts/notificationContext';
 import { AuthProvider } from './contexts/authContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <NotificationProvider>
-        <App />
-      </NotificationProvider>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <NotificationProvider>
+          <App />
+        </NotificationProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
