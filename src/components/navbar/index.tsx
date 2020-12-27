e54858db3d6055725c6a9dd9b73180b70fb7208d
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-26 22:14:23 -0300

    feat(navbar): add navigation links

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { CgProfile } from 'react-icons/cg';
 import { ImExit } from 'react-icons/im';
 import { useAuth } from '../../contexts/authContext';
 import * as S from './styles';
 
 export default () => {
-  const { token } = useAuth();
+  const { token, signOut } = useAuth();
 
   return (
     <S.NavbarContainer>
-      <S.Title>S.N.I. - Survival Network Interface</S.Title>
+      <Link to="/">
+        <S.Title>S.N.I. - Survival Network Interface</S.Title>
+      </Link>
       {token && (
       <S.IconButtonContainer>
-        <S.IconButton>
-          <CgProfile />
-        </S.IconButton>
-        <S.IconButton>
+        <Link to="/user-profile">
+          <S.IconButton>
+            <CgProfile />
+          </S.IconButton>
+        </Link>
+        <S.IconButton onClick={signOut}>
           <ImExit />
         </S.IconButton>
       </S.IconButtonContainer>
