cb0365dbadfa2b18b4b424a4ea22610c2bc5c7c1
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-04 13:48:45 -0300

    feat(survivor details): integrate Leaflet map

--- src/components/survivorDetails/index.tsx
+++ src/components/survivorDetails/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { LatLng } from 'leaflet';
 import { useModules } from '../../contexts/modulesContext';
 import { useSurvivor } from '../../contexts/survivorContext';
 import { Block, Container, Title } from '../../generalStyles';
 
 export default () => {
+  const [position, setPosition] = useState<LatLng | null>(null);
   const { survivor } = useSurvivor();
   const { toggleModule } = useModules();
+  const { location } = survivor || {};
+
+  useEffect(() => {
+    setPosition(null);
+    if (location) {
+      setPosition({
+        lat: location.latitude,
+        lng: location.longitude
+      } as unknown as LatLng);
+    }
+  }, [location]);
 
   return survivor && (
     <Container>
@@ -16,7 +30,16 @@ export default () => {
         &apos;s details
       </Title>
       <Block>
-        <p>there will be details here, eventually</p>
+        {position && (
+          <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '200px' }}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position} />
+          </MapContainer>
+        )}
+        {!position && <p>This user hasn&apos;t set his location</p>}
       </Block>
     </Container>
   );
