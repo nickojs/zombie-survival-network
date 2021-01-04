12cd65e5a96e5f40c0df09a55a6cf0838a3001db
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-04 04:25:17 -0300

    fix(constants): fix 'below' math

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -2,5 +2,5 @@ const CONTAINER_WIDTH = 400;
 const CONTAINER_HEIGHT = 500;
 
 export const right = window.innerWidth - CONTAINER_WIDTH;
-export const below = window.innerHeight - CONTAINER_HEIGHT;
+export const below = window.innerHeight - CONTAINER_HEIGHT - 45;
 export const left = 0;
