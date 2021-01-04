82a9d12515680c107292d86ce570825f028a30a2
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-03 23:53:14 -0300

    feat(dock): add modules context hooks into dock, to toggle desktop items

--- src/components/dock/index.tsx
+++ src/components/dock/index.tsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdArrowDropUp, MdArrowDropDown } from 'react-icons/md';
+import { useModules, Module } from '../../contexts/modulesContext';
 
 import * as S from './styles';
 
 export default () => {
   const [menu, toggleMenu] = useState<boolean>(false);
-
+  const [dockedModules, setDockedModules] = useState<Module[]>([]);
   const menuHandler = () => toggleMenu(!menu);
+
+  const { modules, toggleModule } = useModules();
+
+  useEffect(() => {
+    const filterModules = modules.filter((mo) => !mo.display && mo.isDockable);
+    setDockedModules(filterModules);
+  }, [modules]);
 
   return (
     <S.Footer>
@@ -17,7 +25,18 @@ export default () => {
         {menu ? <MdArrowDropDown /> : <MdArrowDropUp />}
       </S.ToggleButton>
       <S.FootContainer show={menu ? 1 : 0}>
-        {/* minimized modules */}
+        {dockedModules.map((module) => {
+          const { id } = module;
+          return (
+            <button
+              type="button"
+              key={id}
+              onClick={() => toggleModule(id)}
+            >
+              {id}
+            </button>
+          );
+        })}
       </S.FootContainer>
     </S.Footer>
   );
