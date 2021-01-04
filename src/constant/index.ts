9751230801f40f930ec8eb7464b649b7d9aff7e9
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-04 10:25:10 -0300

    feat(constants): add 'middle' constant

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,8 @@
 const CONTAINER_WIDTH = 400;
 const CONTAINER_HEIGHT = 500;
+const BOTTOM_HEIGHT = 45;
 
 export const right = window.innerWidth - CONTAINER_WIDTH;
-export const below = window.innerHeight - CONTAINER_HEIGHT - 45;
+export const below = window.innerHeight - CONTAINER_HEIGHT - BOTTOM_HEIGHT;
+export const middle = (window.innerWidth / 2) - (CONTAINER_WIDTH / 2);
 export const left = 0;
