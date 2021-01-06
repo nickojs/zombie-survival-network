99c1554c9bc1eddcff4ba920ff39d6c706ed2699
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-06 12:45:10 -0300

    feat(context/modules): create 'updatePosition' method, triggers on drag end

--- src/contexts/modulesContext.tsx
+++ src/contexts/modulesContext.tsx
@@ -1,4 +1,6 @@
-import React, { FunctionComponent, useContext, useState } from 'react';
+import React, {
+  FunctionComponent, useContext, useState, useEffect
+} from 'react';
 import { below, middle, right } from '../constant';
 
 // available modules
@@ -67,6 +69,12 @@ interface ModulesContextProps {
   modules: Module[];
   toggleModule(moduleId: string): void;
   isSelected(moduleId: string): boolean;
+  updatePosition(moduleId: string, position: Position): void;
+}
+
+export interface Position {
+  x: number;
+  y: number;
 }
 
 export const ModulesContext = React.createContext<ModulesContextProps>(
@@ -76,15 +84,22 @@ export const ModulesContext = React.createContext<ModulesContextProps>(
 export const ModulesProvider: React.FC = ({ children }) => {
   const [modules, setModules] = useState<Module[]>([...Modules]); // setting default modules for now
 
-  const toggleModule = (moduleId: string) => {
+  const replaceModule = (moduleId: string): [Module | undefined, Module[], number] => {
     const copyState = [...modules];
     const module = copyState.find((module) => module.id === moduleId);
     const moduleIndex = copyState.findIndex((module) => module.id === moduleId);
 
-    if (!module || moduleIndex === -1) return;
-    module.display = !module.display;
-    copyState.splice(moduleIndex, 1, module);
-    setModules(copyState);
+    return [module, copyState, moduleIndex];
+  };
+
+  const toggleModule = (moduleId: string) => {
+    const [module, modules, moduleIndex] = replaceModule(moduleId);
+
+    if (module) {
+      module.display = !module.display;
+      modules.splice(moduleIndex, 1, module);
+      setModules(modules);
+    }
   };
 
   const isSelected = (moduleId: string) => {
@@ -93,12 +108,36 @@ export const ModulesProvider: React.FC = ({ children }) => {
     return module.display;
   };
 
+  const updatePosition = (moduleId: string, position: Position) => {
+    const [module, modules, moduleIndex] = replaceModule(moduleId);
+
+    if (module) {
+      module.screenPos = position;
+      modules.splice(moduleIndex, 1, module);
+      setModules(modules);
+    }
+  };
+
+  const saveModulesPos = () => {
+    const copyState = [...modules];
+    localStorage.setItem('modules', JSON.stringify(copyState));
+  };
+
+  useEffect(() => {
+    // get existing screnPos from localStorage
+  }, []);
+
+  useEffect(() => {
+    saveModulesPos();
+  }, [modules]);
+
   return (
     <ModulesContext.Provider
       value={{
         modules,
         toggleModule,
-        isSelected
+        isSelected,
+        updatePosition
       }}
     >
       {children}
