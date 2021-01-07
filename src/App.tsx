8fcd8425e92f6782c387c4808ceea2fc5c1e8b03
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-07 00:18:55 -0300

    fix(app.tsx): toggles Dock based on user token

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import Notification from './components/notification';
 import Navbar from './components/navbar';
 import Dock from './components/dock';
 import Desktop from './Containers/Desktop';
+import { useAuth } from './contexts/authContext';
 
 function App() {
+  const { token } = useAuth();
+
   return (
     <>
       <Notification />
       <Navbar />
       <Desktop />
-      <Dock />
+      {token && <Dock />}
     </>
   );
 }
