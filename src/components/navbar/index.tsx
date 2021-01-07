21e350889dafc203554b5a5fe6b79a074165228f
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-06 22:06:44 -0300

    feat(navbar): integrate new ModulesContext methods

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { CgProfile } from 'react-icons/cg';
 import { ImExit } from 'react-icons/im';
 import { MdGpsFixed } from 'react-icons/md';
+import { AiOutlineSave } from 'react-icons/ai';
 
 import { useAuth } from '../../contexts/authContext';
 import { useModules } from '../../contexts/modulesContext';
@@ -10,7 +11,9 @@ import * as S from './styles';
 
 export default () => {
   const { token, signOut } = useAuth();
-  const { toggleModule, isSelected } = useModules();
+  const {
+    toggleModule, isSelected, uploadModulesPos, loading
+  } = useModules();
 
   return (
     <S.NavbarContainer>
@@ -20,16 +23,28 @@ export default () => {
         <S.IconButton
           selected={isSelected('profile') ? 1 : 0}
           onClick={() => toggleModule('profile')}
+          title="Open Profile panel"
         >
           <CgProfile />
         </S.IconButton>
         <S.IconButton
           selected={isSelected('location') ? 1 : 0}
           onClick={() => toggleModule('location')}
+          title="Open Location panel"
         >
           <MdGpsFixed />
         </S.IconButton>
-        <S.IconButton onClick={signOut}>
+        <S.IconButton
+          selected={loading ? 1 : 0}
+          onClick={uploadModulesPos}
+          title="Save containers current position"
+        >
+          <AiOutlineSave />
+        </S.IconButton>
+        <S.IconButton
+          onClick={signOut}
+          title="Exit the application"
+        >
           <ImExit />
         </S.IconButton>
       </S.IconButtonContainer>
