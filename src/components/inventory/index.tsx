03ab8f35fcc4f8024044f881f1857dd6a39b17bd
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-09 02:51:06 -0300

    feat(inventory): add a inner menu and a searchbar to fetch new items

--- src/components/inventory/index.tsx
+++ src/components/inventory/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { GiBroadDagger } from 'react-icons/gi';
 
 import { useAuth } from '../../contexts/authContext';
-import { Title } from '../../generalStyles';
-import * as S from './styles';
 import { NotificationTypes, useNotification } from '../../contexts/notificationContext';
+
+import * as S from './styles';
 
 interface OSRSItem {
   _id: string;
@@ -12,17 +13,45 @@ interface OSRSItem {
   icon: string;
 }
 
+const InventoryItems: React.FC<{ items: OSRSItem[], search: boolean }> = ({ items, search }) => {
+  const itemClickHandler = () => {
+    if (search) {
+      console.log('im a search item!');
+    }
+  };
+
+  return (
+    <S.ItemWrapper>
+      {items.map((item) => (
+        <S.Item key={item._id} onClick={itemClickHandler}>
+          <S.ItemImage
+            alt={item.name}
+            title={item.name}
+            src={`data:image/png;base64,${item.icon}`}
+          />
+        </S.Item>
+      ))}
+    </S.ItemWrapper>
+  );
+};
+
 export default () => {
   const [inventory, setInventory] = useState<OSRSItem[]>([]);
+  const [searchResult, setSearchResult] = useState<OSRSItem[]>([]);
+
+  const [search, setSearch] = useState<string>('');
+  const [searchMode, setSearchMode] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
 
   const { messageHandler } = useNotification();
-
   const { user } = useAuth();
   const { items } = user || { };
 
   const fetchOSRSItem = (itemId: string) => axios
     .get(`https://api.osrsbox.com/items?where={ "_id": "${itemId}", "duplicate": false }`);
+
+  const fetchOSRSItems = (name: string) => axios
+    .get(`https://api.osrsbox.com/items?where={ "$text" :{ "$search": "${name}" }, "duplicate": false }`);
 
   useEffect(() => {
     if (items) {
@@ -43,21 +72,43 @@ export default () => {
     }
   }, [items]);
 
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(async () => {
+      const request = await fetchOSRSItems(search);
+      const { data } = request;
+      const items = data._items.slice(0, 24);
+      setSearchResult(items);
+      setLoading(false);
+    }, 500);
+
+    return () => { clearTimeout(timer); };
+  }, [search]);
+
   return (
-    <div>
-      <Title>Inventory</Title>
-      <S.InventoryContainer>
-        {loading
-          ? <p>Loading items...</p>
-          : inventory.map((item) => (
-            <S.Item>
-              <S.ItemImage
-                alt={item.name}
-                src={`data:image/png;base64,${item.icon}`}
-              />
-            </S.Item>
-          ))}
-      </S.InventoryContainer>
-    </div>
+    <S.InventoryContainer>
+
+      <S.InventoryMenu>
+        <S.InventoryMenuItem
+          title="Fetch and add items to the inventory"
+          onClick={() => setSearchMode(!searchMode)}
+        >
+          <GiBroadDagger />
+        </S.InventoryMenuItem>
+      </S.InventoryMenu>
+
+      {searchMode && (
+        <S.SearchBar
+          type="text"
+          placeholder="search items"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      )}
+
+      {loading
+        ? <p>Loading items...</p>
+        : <InventoryItems items={searchMode ? searchResult : inventory} search={searchMode} />}
+
+    </S.InventoryContainer>
   );
 };
--- src/components/inventory/styles.ts
+++ src/components/inventory/styles.ts
@@ -3,20 +3,57 @@ import inventoryBg from '../../assets/inventory.png';
 
 export const InventoryContainer = styled.div`
   position: relative;
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: flex-start;
-
   height: 485px;
   width: 350px;
   
   padding: 60px 50px;
-  margin: 24px; margin-top: -24px;
+  margin: 24px;
 
   background-image: url(${inventoryBg});
   background-size: contain;
   background-position: center;
   background-repeat: no-repeat;
+`;
+
+export const InventoryMenu = styled.div`
+  position: absolute;
+  top: 0; left: 0; right: 0;
+  height: 50px;
+  
+  display: flex;
+  flex-direction: row;
+`;
+
+export const InventoryMenuItem = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  width: 12.5%;
+  padding: 4px; 
+  
+  font-size: 24px;
+  background: white;
+  border-radius: 8px;
+
+  cursor: pointer;
+`;
+
+export const SearchBar = styled.input`
+  width: 100%; 
+  height: 32px;
+  
+  text-align: center;
+  
+  border: 1px inset gray;
+  border-radius: 4px;
+  color: white;
+`;
+
+export const ItemWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
 `;
 
 export const Item = styled.div`
