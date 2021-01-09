d0c01f2d765947eecbdb1bc13940904776e50b1e
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-09 04:52:49 -0300

    feat(inventory/item.tsx): extract inventoryItems's Item to its own component

---
+++ src/components/inventory/item.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { OSRSItem } from '.';
+import * as S from './styles';
+
+interface ItemProps {
+  item: OSRSItem;
+  search: boolean;
+  searchClick: (item: OSRSItem) => void;
+  deleteClick: (item: OSRSItem) => void;
+}
+
+export default ({
+  item, search, searchClick, deleteClick
+}: ItemProps) => {
+  const [show, setShow] = useState<boolean>(false);
+  const [triggerDelete, setTriggerDelete] = useState<boolean>(false);
+
+  const deleteItemHandler = () => {
+    if (triggerDelete) deleteClick(item);
+    setTriggerDelete(!triggerDelete);
+  };
+
+  const showDeleItemHandler = () => { setShow(!show); };
+
+  useEffect(() => {
+    if (!triggerDelete) return;
+
+    const timer = setTimeout(() => {
+      setTriggerDelete(false);
+    }, 1500);
+
+    return () => { clearTimeout(timer); };
+  }, [triggerDelete]);
+
+  return (
+    <S.Item
+      onMouseEnter={showDeleItemHandler}
+      onMouseLeave={showDeleItemHandler}
+      onClick={() => searchClick(item)}
+    >
+      <S.ItemImage
+        alt={item.name}
+        title={item.name}
+        src={`data:image/png;base64,${item.icon}`}
+      />
+      {!search && (
+        <S.RemoveItem
+          show={show ? 1 : 0}
+          triggered={triggerDelete ? 1 : 0}
+          onClick={deleteItemHandler}
+        >
+          {triggerDelete ? '!' : 'X'}
+        </S.RemoveItem>
+      )}
+    </S.Item>
+  );
+};
--- src/components/inventory/styles.ts
+++ src/components/inventory/styles.ts
@@ -43,7 +43,7 @@ export const SearchBar = styled.input`
   width: 100%; 
   height: 32px;
   margin-bottom: 12px;
-  
+
   text-align: center;
   
   border: 1px inset gray;
@@ -65,8 +65,10 @@ export const ItemWrapper = styled.div<ItemWrapperProps>`
   display: flex;
   flex-flow: row wrap;
   justify-content: flex-start;
+  
+  margin-bottom: 8px;
+  
   ${({ disabled }) => disabled && disabledDiv}
-
 `;
 
 export const Item = styled.div`
@@ -85,4 +87,40 @@ export const ItemImage = styled.img`
   transition: .5s;
   height: 50px;
   width: 50px;
+`;
+
+interface RemoveItemProps {
+  show: number;
+  triggered: number;
+}
+
+const showButton = css`
+  visibility: visible;
+  opacity: 1;
+`;
+
+export const RemoveItem = styled.button<RemoveItemProps>`
+  position: absolute;
+  bottom: 0; right: 0;
+  
+  width: 20px;
+  padding: 4px;
+  
+  font-size: 10px;
+  text-align: center;
+  
+  border: 1px solid black;
+  border-radius: 100%;
+  
+  color: black;
+  background-color: ${({ triggered }) => (triggered ? 'yellow' : 'red')};
+  box-shadow: 2px 2px 4px black;
+  
+  transition: color .3s, visibility .3s, opacity .3s;
+  
+  visibility: hidden;
+  opacity: 0;
+  cursor: pointer;
+  
+  ${({ show }) => show && showButton};
 `;
